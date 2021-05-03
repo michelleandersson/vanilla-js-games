815e06808b8bde704d5d470bd547dbeb20b2b938
Michelle Andersson <michelleandersson@live.com>
2021-05-03 11:55:52 +0200

    create gameturn function in simon

--- simonSays/simon.js
+++ simonSays/simon.js
@@ -57,5 +57,29 @@ function play() {
     for (var i = 0; i < 20; i++) {
         order.push(Math.floor(Math.random() * 4) + 1); 
     }
-    console.log(order); 
+    compTurn = true; 
+    intervalId = setInterval(gameTurn, 800); 
 };
+
+function gameTurn () {
+    //ensures player can't click buttons while computer is flashing
+   on = false;  
+
+   if (flash == turn) {
+       clearInterval(intervalId); 
+       compTurn = false; 
+       clearColor(); 
+       on = true; 
+   }
+
+   if (compTurn) {
+       clearColor();
+       setTimeout(() => {
+        if (order[flash] == 1) one();     
+        if (order[flash] == 2) two();   
+        if (order[flash] == 3) three();   
+        if (order[flash] == 4) four();   
+        flash++; 
+       }, 200);  
+   }
+}; 
