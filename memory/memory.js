78d169db5f68a4193b77f03de133dc0f9caca6da
Michelle Andersson <michelleandersson@live.com>
2021-05-26 09:53:06 +0200

    create card array and board function

---
+++ memory/memory.js
@@ -0,0 +1,70 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    //card options
+    const cardArray = [
+        {
+            name: 'bird1', 
+            img: 'images/bird1.png'
+        }, 
+        {
+            name: 'bird1', 
+            img: 'images/bird1.png'
+        }, 
+        {
+            name: 'bird2', 
+            img: 'images/bird2.png'
+        }, 
+        {
+            name: 'bird2', 
+            img: 'images/bird2.png'
+        }, 
+        {
+            name: 'bird3', 
+            img: 'images/bird3.png'
+        }, 
+        {
+            name: 'bird3', 
+            img: 'images/bird3.png'
+        }, 
+        {
+            name: 'bird4', 
+            img: 'images/bird4.png'
+        }, 
+        {
+            name: 'bird4', 
+            img: 'images/bird4.png'
+        }, 
+        {
+            name: 'bird5', 
+            img: 'images/bird5.png'
+        }, 
+        {
+            name: 'bird5', 
+            img: 'images/bird5.png'
+        }, 
+        {
+            name: 'bird6', 
+            img: 'images/bird6.png'
+        }, 
+        {
+            name: 'bird6', 
+            img: 'images/bird6.png'
+        }, 
+
+    ]
+
+    const grid = document.querySelector('.grid'); 
+
+    //create game board
+    function createBoard() {
+        for (let i = 0; i < cardArray.length; i++) {
+            let card = document.createElement('img')
+            card.setAttribute('src', 'images/blank.png')
+            card.setAttribute('data-id', i)
+            // card.addEventListener('click', flipcard)
+            grid.appendChild(card)
+        }
+    }
+
+    createBoard(); 
+})
